6d41d0f3ec98c6d1250e02e92a8163102629e8ee
maxwel.x <ferramentasite@gmail.com>
2026-06-23 16:55:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,35 @@
 const gallery = document.getElementById("gallery");
-const searchInput = document.getElementById("searchInput");
 
-let photos = [];
+for(let i = 8155; i <= 8442; i++){
 
-fetch("fotos.json")
-.then(response => response.json())
-.then(data => {
+    gallery.innerHTML += `
 
-    photos = data;
+    <div class="card">
 
-    renderPhotos(photos);
-});
+        <div class="image-container">
 
-function renderPhotos(list){
+            <img src="fotos/IMG_${i}.JPG" alt="IMG_${i}">
 
-    gallery.innerHTML = "";
-
-    list.forEach((photo,index)=>{
-
-        const number = String(index+1).padStart(3,'0');
-
-        gallery.innerHTML += `
-
-        <div class="card">
-
-            <div class="image-container">
-
-                <img src="fotos/${photo}" alt="${number}">
-
-                <div class="watermark"></div>
-
-            </div>
-
-            <div class="info">
-
-                <h3>Foto #${number}</h3>
-
-                <div class="preco">R$ 5,00</div>
-
-                <a
-                class="btn"
-                target="_blank"
-                href="https://wa.me/5593999999999?text=Olá,%20quero%20comprar%20a%20foto%20${number}">
-                Comprar
-                </a>
-
-            </div>
+            <div class="watermark"></div>
 
         </div>
-        `;
-    });
+
+        <div class="info">
+
+            <h3>IMG_${i}</h3>
+
+            <div class="preco">R$ 5,00</div>
+
+            <a
+            class="btn"
+            target="_blank"
+            href="https://wa.me/5593991606690?text=Olá,%20quero%20comprar%20a%20foto%20IMG_${i}">
+            Comprar
+            </a>
+
+        </div>
+
+    </div>
+
+    `;
 }
-
-searchInput.addEventListener("input",()=>{
-
-    const termo = searchInput.value.toLowerCase();
-
-    const filtradas = photos.filter((foto,index)=>{
-
-        const numero = String(index+1).padStart(3,'0');
-
-        return numero.includes(termo);
-    });
-
-    renderPhotos(filtradas);
-});
